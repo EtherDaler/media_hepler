3e8f2043e872fd7d0a61fdb0753c80228a7c252a
EtherDaler <daler.hojimatov@yandex.ru>
2024-11-03 03:58:56 +0300

    fix send video

--- bot_commands.py
+++ bot_commands.py
@@ -242,8 +242,9 @@ async def get_link(message: Message, state: FSMContext) -> None:
         filename = await worker.download_from_youtube(link)
         if filename:
             try:
-                video_file = InputFile(video=f"./videos/youtube/{filename}")
-                doc = await message.bot.send_video(message.chat.id, video_file, caption='Ваше видео готово!\n@django_media_helper_bot')
+                doc = await message.answer_video(video=f"./videos/youtube/{filename}", caption='Ваше видео готово!\n@django_media_helper_bot')
+                #video_file = InputFile(video=f"./videos/youtube/{filename}")
+                #doc = await message.bot.send_video(message.chat.id, video_file, caption='Ваше видео готово!\n@django_media_helper_bot')
                 #doc = await message.answer_document(document=FSInputFile(f"./videos/youtube/{filename}"), caption="Ваше видео готово!\n@django_media_helper_bot")
                 await message.bot.send_message(chat_id=config.DEV_CHANEL_ID, text=f"Пользователь @{username} (ID: {user_id}) успешно скачал видео из #YouTube")
                 if doc:
