b25e04b50be6aca3348b06e43f4e170664b977ad
Dalik <daler.safarovv@yandex.ru>
2024-11-05 23:47:36 +0300

    fix

--- worker.py
+++ worker.py
@@ -109,7 +109,7 @@ async def download_from_youtube(link, path='./videos/youtube', out_format="mp4",
     po_token = "MnTT_c32vPYUIdPFFRKfxFLG21j22_tHNgtcxsnyI-BBLV8qkeyHs5ymawmenUy_VXvcmiGSA6BKQOwOf97daFTOMr0L_WimcA4MsiCKOaeiCiySQd0Ia15Asyt8gsbyVM9jsjIqjHnuFqYJPqAMaqeT1oPnuA=="
     bad_characters = '\/:*?"<>|'
     ydl_opts = {
-        'format': 'best[ext=mp4]/bestvideo[ext=mp4]+bestaudio[ext=m4a]/best',  # Выбор лучшего доступного формата MP4
+        'format': '22/18/best',
         'outtmpl': f'{path}/%(title)s.%(ext)s',
         'noplaylist': True,
         'cookiefile': './cookies.txt',
