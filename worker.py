104dba46d3f9c3bca11fb04297c8a952f72f5180
Daler Hojimatov <63519670+EtherDaler@users.noreply.github.com>
2024-11-05 11:58:57 +0300

    Update worker.py fix quote

--- worker.py
+++ worker.py
@@ -126,9 +126,9 @@ async def download_from_youtube(link, path='./videos/youtube', out_format="mp4",
         video_filename = f"{video_title}.{result['ext']}"  # Форматирование имени файла
         # Если файл не в формате mp4, конвертируем его в mp4
         if result['ext'] != 'mp4':
-            output_file = os.path.join(f"{video_title}.mp4")
+            output_file = os.path.join(path, f"{video_title}.mp4")
             try:
-                downloaded_file_escaped = shlex.quote(os.path.abspath(f"{video_filename}"))
+                downloaded_file_escaped = shlex.quote(os.path.abspath(f"{path}/{video_filename}"))
                 output_file_escaped = shlex.quote(os.path.abspath(f"{output_file}"))
                 print(f"Downloaded file path: {downloaded_file_escaped}")
                 print(f"Output file path: {output_file_escaped}")
