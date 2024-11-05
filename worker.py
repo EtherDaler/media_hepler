fae6227a6ac560416e17a0cb244c7a12fff7eef6
Daler Hojimatov <63519670+EtherDaler@users.noreply.github.com>
2024-11-05 11:32:28 +0300

    Update worker.py fix filename in yt download

--- worker.py
+++ worker.py
@@ -127,12 +127,12 @@ async def download_from_youtube(link, path='./videos/youtube', out_format="mp4",
         if result['ext'] != 'mp4':
             output_file = os.path.join(path, f"{video_title}.mp4")
             try:
-                ffmpeg.input(downloaded_file).output(output_file).run()
-                os.remove(downloaded_file)  # Удаляем оригинальный файл, если он не в mp4
+                ffmpeg.input(video_filename).output(output_file).run()
+                os.remove(video_filename)  # Удаляем оригинальный файл, если он не в mp4
                 return output_file  # Возвращаем путь к mp4 файлу
             except ffmpeg.Error as e:
                 print(f"FFmpeg error: {e}")
-        compressed_filename = f"{video_title}-compressed.{out_format}"
+        #compressed_filename = f"{video_title}-compressed.{out_format}"
         #filename = compress_video_ffmpeg(video_filename, compressed_filename)
         return video_filename
     return None
