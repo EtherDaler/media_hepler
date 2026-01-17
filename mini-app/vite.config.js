d06b2cea091b13d54b8f6a95c75c1195ac1c9484
Daler Khodzhimatov <khodzhimatovd@172.31.135.120-vpn.dhcp.yndx.net>
2026-01-17 19:18:25 +0300

    fix hosts

--- mini-app/vite.config.js
+++ mini-app/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: '0.0.0.0',
-    allowedHosts: ['.ngrok-free.app', '.ngrok.io'],
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8001',
