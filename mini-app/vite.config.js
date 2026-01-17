c5212405d20f1543c93b58d9e092ada4d6ee5e40
Daler Khodzhimatov <khodzhimatovd@10.211.13.144-vpn.dhcp.yndx.net>
2026-01-17 06:35:47 +0300

    fix port for miniapp

--- mini-app/vite.config.js
+++ mini-app/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8001',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
